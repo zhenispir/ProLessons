43ec57da1f5897db0b3e242d888b19859e26374a
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-02-28 18:34:20 +0500

    int & input together

--- Islambek/2-sabaq.py
+++ Islambek/2-sabaq.py
@@ -1,4 +1,15 @@
-x = input() # data type: str
-            #x = "Salem"
+"""
+x = input() #Salem
+#x= "Salem"
+print(x*3)
 print(x*5)
-print(x)
+print(x*7)
+"""
+a = int(input())
+x = int(input()) 
+y = float(input())
+print(a * x + y)
+
+10 <= input
+11 <= output
+9  <= output
