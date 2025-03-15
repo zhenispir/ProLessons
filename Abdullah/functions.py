dc825755ee707c3e7171dbd25686a0863498f852
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-03-15 13:02:19 +0500

    Using function in the competitive programming

--- Abdullah/functions.py
+++ Abdullah/functions.py
@@ -15,10 +15,18 @@ def audan(a,b):     # argumentter
     area = a*b / 2
     return area     # kurastyru
 print(audan(5,6))      # koldanu
-'''
+
 
 def audan(a, b=10):     # argumentter, b = default
     area = a*b / 2
     return area     # kurastyru
 print(audan(5,6))      # koldanu
 print(audan(5))     # b = 10 autamattyk
+'''
+
+# olimpiadada funksiya koldanu
+def audan(a,b):
+    area  =  a*b // 2
+    return area
+
+print(audan(int(input()),int(input())))
