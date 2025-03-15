b2c9cd97752cac65d5a4e15c2e458702c39e13a4
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-03-15 12:25:55 +0500

    Exaplaining basics of function

---
+++ Abdullah/functions.py
@@ -0,0 +1,51 @@
+def audan():
+    a = int(input())
+    b = int(input())
+    area = a*b / 2
+    return area     # kurastyru
+print(audan())      # koldanu
+
+
+# 18 joldan => 11 jolga
+# 12 ainymnaly => 2 ainynamly
+# bug 18 joldan => 5 jolga
+
+
+
+
+#print(audan())
+
+
+#
+#
+#
+
+
+
+
+#print(audan())
+
+
+
+
+#print(audan())
+
+
+#
+#
+#
+
+
+
+
+#print(audan())
+
+
+#
+#
+#
+
+
+
+
+#print(audan())
