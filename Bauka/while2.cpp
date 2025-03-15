38541db3312f5e7919d320f6d26dbf6c9432f4be
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-03-15 16:33:59 +0500

    Add while loop examples in while1.cpp and while2.cpp for basic iteration

---
+++ Bauka/while2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int a = 0;
+	while (a <= 50){
+		cout << a << endl;
+		++a;
+	}
+	
+	return 0;
+}
