5522a5fa91a9fa3c09e7f2886c5a4d9a1ab48c86
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-03-15 12:43:43 +0500

    argumentter

--- Abdullah/functions.py
+++ Abdullah/functions.py
@@ -1,3 +1,4 @@
+'''
 def audan():
     a = int(input())
     b = int(input())
@@ -5,47 +6,12 @@ def audan():
     return area     # kurastyru
 print(audan())      # koldanu
 
-
 # 18 joldan => 11 jolga
 # 12 ainymnaly => 2 ainynamly
 # bug 18 joldan => 5 jolga
+'''
 
-
-
-
-#print(audan())
-
-
-#
-#
-#
-
-
-
-
-#print(audan())
-
-
-
-
-#print(audan())
-
-
-#
-#
-#
-
-
-
-
-#print(audan())
-
-
-#
-#
-#
-
-
-
-
-#print(audan())
+def audan(a,b):     # argumentter
+    area = a*b / 2
+    return area     # kurastyru
+print(audan(5,6))      # koldanu
