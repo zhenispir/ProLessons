1924e2b7afa9dde8694b776e6d6e79da188c65ae
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-03-15 12:56:43 +0500

    default argument

--- Abdullah/functions.py
+++ Abdullah/functions.py
@@ -9,9 +9,16 @@ print(audan())      # koldanu
 # 18 joldan => 11 jolga
 # 12 ainymnaly => 2 ainynamly
 # bug 18 joldan => 5 jolga
-'''
+
 
 def audan(a,b):     # argumentter
     area = a*b / 2
     return area     # kurastyru
 print(audan(5,6))      # koldanu
+'''
+
+def audan(a, b=10):     # argumentter, b = default
+    area = a*b / 2
+    return area     # kurastyru
+print(audan(5,6))      # koldanu
+print(audan(5))     # b = 10 autamattyk
