b506c662dad3c2ebecc26b80c93bffd3781d42ab
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-03-16 22:30:36 +0500

    Add while loop examples in while3.cpp, while4.cpp, and while5.cpp for iterative output

---
+++ Bauka/while3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int i = 0;	// starting point
+	
+	while (i <= 20) {	// condition (stopping point)
+		cout << i << '\t' << i+2 << '\n';
+		i += 1;  // incremention
+	}
+	
+	return 0;
+}
---
+++ Bauka/while4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int i = 0;
+	while (i <= 10){
+		cout << i << '\t' << (i+1)*5 << endl;
+		i+=1;
+	}
+	
+	return 0;
+}
