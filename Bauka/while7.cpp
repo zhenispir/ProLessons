042f00dcb0aac0a01003224c887eefb801846e28
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-03-20 15:34:07 +0500

    while loop homeworks are done.

---
+++ Bauka/while7.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int i = 0;
+	while (i <= 5){
+		cout << i << '\t' << pow(2, i) << endl;
+		++i;
+	}
+	
+	return 0;
+}
