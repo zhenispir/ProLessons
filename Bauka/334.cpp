b6c069865a1f786d36e979381d848e080015f5d0
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-03-20 16:03:14 +0500

    Solving tasks from Informatics:  333, 334 and 335

---
+++ Bauka/334.cpp
@@ -0,0 +1,71 @@
+// C++ => bits/stdc++.h separately included headers
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+using namespace std;
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	for (int i = a; i < b+1; ++i){
+		if ( i % d == c){
+			cout << i << '\n';
+		}
+	}
+
+    return 0;
+}
