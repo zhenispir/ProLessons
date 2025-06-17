661b9a357f7f2f6532e5093f3f386aa918c80665
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-06-17 19:53:34 +0500

    Update sabaq_05

--- Ernar/sabaq05_bits.cpp
+++ Ernar/sabaq05_bits.cpp
@@ -8,8 +8,8 @@ int main() {
 	unsigned char a, b, c;
 	a = 14;  // 1 1 1 0
 	b = 10;  // 1 0 1 0
-	c = int(a & b); // AND 
-	cout << c; //  1 0 1 0
+	c = a & b; // AND 
+	cout << c; // 1 0 1 0
 	
 	return 0;
 }
