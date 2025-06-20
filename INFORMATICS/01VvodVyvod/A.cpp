66e27ba98c22e35c1f5a8b881faa158474ebf849
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-06-20 17:50:29 +0500

    Created INFORMATICS folder

---
+++ INFORMATICS/01VvodVyvod/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int a,b,c;
+	cin >> a>>b;
+	c = sqrt(a*a+b*b);
+	cout << c;
+	return 0;
+}
