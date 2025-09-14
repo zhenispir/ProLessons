80631ce23d7f9e54ad64d18325b573e7a516ea95
Zhenis Pirnepessov <yenispiro@gmail.com>
2025-09-14 20:11:24 +0500

    char review

---
+++ ACMP.ru/char_review.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	char n = 'A', p; // n, p bir simvol
+	int y='3'; // simvoldyng sany
+	
+	y = n;
+	p = n+3;
+	
+	cout << y << " - bul y";
+	cout << '\n';
+	cout << p << " - bul p";
+	
+	return 0;
+}
+
